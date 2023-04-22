12ba0330f3a8095c4c93e427de16f76a1606b320
j0zeph <notaj@tutanota.de>
2023-04-23 01:59:22 +0300

    Format progress utils code.

--- utils/progress_utils.py
+++ utils/progress_utils.py
@@ -6,17 +6,18 @@ from rich.progress import Progress
 PROGRESS_BAR_INCREASE = 0.05
 BAR_COLOR = 'green'
 
-def show_progress_bar(message: str, total: Optional[int]=None) -> None:
+
+def show_progress_bar(message: str, total: Optional[int] = None) -> None:
     """Shows a progress bar
-    
+
     Parameters
     ----------
     message : str
         The message to be displayed, alongside the progress bar.
     total : int or None, Optional
         The total size of the information being downloaded.
-        
-        When total is provided, a "determinate" progress bar is displayed. 
+
+        When total is provided, a "determinate" progress bar is displayed.
         An "indeterminate" progress bar is displayed if no total is given.
     """
     with Progress() as progress:
