c843fcb3ef59c65ad18ec91966744f3d57f32e9f
j0zeph <josephamukun@tutanota.com>
2023-04-26 15:13:15 +0300

    Add test for csv file with no required columns
    A .csv file which lacks the required headers is
    not a correct format.

--- tests/test_huesort.py
+++ tests/test_huesort.py
@@ -1,6 +1,19 @@
 from utils.csv_utils import *
 
+
 def test_csv_cannot_be_empty(tmp_path):
     csv_file = tmp_path / 'empty.csv'
     csv_file.write_text('')
+    assert not is_valid_letterboxd_format(csv_file)
+
+
+def test_non_empty_csv_must_contain_required_columns(tmp_path):
+    content = """
+    Letterboxd list export v7,,,,
+    Date,Name,Tags,URL,Description
+    2023-04-17,A test lis,Sort by colour,The url,A test list.
+    ,,,,
+    """.strip()
+    csv_file = tmp_path / 'no_columns.csv'
+    csv_file.write_text(content)
     assert not is_valid_letterboxd_format(csv_file)
