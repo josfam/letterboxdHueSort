d2a74e94cf88a4d34b7d49980e858cc1c18e279c
j0zeph <josephamukun@tutanota.com>
2023-04-26 19:49:10 +0300

    Test that required columns can appear in any order
    The required columns. `Name`, `Year`, and `URL` can appear in
    any order in the provided csv file.

--- tests/test_huesort.py
+++ tests/test_huesort.py
@@ -1,4 +1,5 @@
 from utils.csv_utils import *
+import itertools
 
 
 def test_csv_cannot_be_empty(tmp_path):
@@ -17,3 +18,18 @@ def test_non_empty_csv_must_contain_required_columns(tmp_path):
     csv_file = tmp_path / 'no_columns.csv'
     csv_file.write_text(content)
     assert not is_valid_letterboxd_format(csv_file)
+
+
+def test_required_columns_can_occur_in_any_order(tmp_path):
+    arrangements = itertools.permutations(['Position', 'Name', 'Year', 'URL', 'Description'])
+    for arrangement in arrangements:
+        content = f"""
+        Letterboxd list export v7,,,,
+        Date,Name,Tags,URL,Description
+        2023-04-17,A test lis,Sort by colour,The url,A test list.
+        ,,,,
+        {','.join(arrangement)}
+        """
+        csv_file = tmp_path / 'columns_in_some_order.csv'
+        csv_file.write_text(content)
+        assert is_valid_letterboxd_format(csv_file)
