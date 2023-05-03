2ffbb9baf1a31c0bf43d1bdd4a9617ac994d39af
j0zeph <josephamukun@tutanota.com>
2023-05-03 09:17:41 +0300

    Refactor poster-getting logic into a function
    The main logic for searching for, and downloading posters
    has been moved into a function.

--- letterboxdHueSort.py
+++ letterboxdHueSort.py
@@ -16,6 +16,7 @@ from utils.poster_utils import (
 )
 import requests
 from rich.console import Console
+from utils.csv_utils import CSVInfo
 
 HARD_ERROR_COLOR = 'deep_pink4'
 IN_PROGRESS_COLOR = 'green'
@@ -27,15 +28,29 @@ parser.add_argument('film_list_csv', help='Absolute path of the csv file represe
 args = parser.parse_args()
 
 film_csv_path: str = args.film_list_csv
+console = Console(color_system='truecolor')
 
 
 def main():
-    console = Console(color_system='truecolor')
     csv_path = get_csv_absolute_path(film_csv_path)
 
     if not is_valid_letterboxd_format(csv_path):
         sys.exit(valid_csv_format_message())
 
+    csv_sections = get_csv_sections(csv_path)
+
+    posters_dir_name = 'posters'  # directory in which to save the posters
+    csv_parent_dir = PurePath(csv_path).parent
+    posters_dir_path = Path(csv_parent_dir) / Path(posters_dir_name)
+
+    if not posters_dir_path.exists():
+        msg = f'\n┣━ {posters_dir_name} folder created\n'
+        posters_dir = create_posters_dir(str(PurePath(csv_path).parent), posters_dir_name, msg)
+        get_posters(csv_sections, Path(poster_dir))
+    elif not all_posters_downloaded(csv_sections, Path(posters_dir_path)):
+        get_posters(csv_sections, posters_dir_path)
+    else:
+        pass
 
 
 def all_posters_downloaded(csv_sections: CSVInfo, posters_dir_path: Path) -> bool:
@@ -58,18 +73,30 @@ def all_posters_downloaded(csv_sections: CSVInfo, posters_dir_path: Path) -> boo
     return poster_count == movie_count
 
 
-    img_extension = '.jpg'  # file extension with which posters will be saved
+def get_posters(csv_sections: CSVInfo, posters_dir: Path):
+    """Finds and downloads the posters for the films listed in the csv file.
 
-    for film in csv_info.film_info:
+    Parameters
+    ----------
+    csv_sections : CSVInfo
+        The three sections of the csv, i.e.`extra_info`, `headers`, and `film_info`.
+    posters_dir : Path
+        The path representing the location of the posters directory, in which posters are downloaded.
+
+    Returns
+    -------
+    None
+    """
+    img_extension = '.jpg'  # file extension with which posters will be saved
+    for film in csv_sections.film_info:
         film_url = film['URL']
         film_name = film['Name']
         print(f'\n ┃ {film_name.upper()}')
 
         # check if the poster already exists before trying to download it
-        if Path.exists(Path(posters_dir) / Path( film_name + img_extension)):
+        if Path.exists(Path(posters_dir) / Path(film_name + img_extension)):
             print(' ┗ Poster already exists. Skipping this film')
             continue
-
         try:
             msg = f' ┃ Searching film page'
             page_contents = get_film_page_html(film_url, msg)
@@ -87,13 +114,15 @@ def all_posters_downloaded(csv_sections: CSVInfo, posters_dir_path: Path) -> boo
                 msg = ' ┃ Fetching poster'
                 poster_content = get_poster_contents(poster_url, msg)
             except requests.exceptions.ConnectionError:
-                console.print(' ━┫ There seems to be a problem with your internet connection.\n', style=HARD_ERROR_COLOR)
+                console.print(
+                    ' ━┫ There seems to be a problem with your internet connection.\n', style=HARD_ERROR_COLOR
+                )
                 sys.exit()
             except KeyboardInterrupt:
                 sys.exit('\n┗━ Goodbye for now ━━━\n')
 
             msg = ' ┗ Saving poster'
-            download_poster(poster_content, film_name, posters_dir, msg, img_extension)
+            download_poster(poster_content, film_name, str(posters_dir), msg, img_extension)
         else:
             console.print(" ━┫ Couldn't find poster", style=SOFT_ERROR_COLOR)
             print()
